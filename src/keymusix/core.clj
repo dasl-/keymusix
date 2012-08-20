9cb04297d943b9196c7f6a99e2f98eb428e8877a
Pepijn de Vos <pepijndevos@gmail.com>
2012-08-20 11:55:23 -0400

    generate mapping

--- src/keymusix/core.clj
+++ src/keymusix/core.clj
@@ -4,52 +4,35 @@
   (:import org.jnativehook.GlobalScreen)
   (:import org.jnativehook.keyboard.NativeKeyListener))
 
-(definst keyboard [volume 1.0 freq 440]
+(definst keyboard [freq 440]
   (let [src (sin-osc freq)
         env (env-gen (perc 0.001 0.3) :action FREE)]
-    (* volume 1 src env)))
+    (* src env)))
 
 (defn play-keyboard [ notename ]
-  (keyboard :freq (* (midi->hz ( note notename)) 4)))
+  (keyboard :freq (* (midi->hz notename))))
 
-(defn map-note [e]
-  (let [k (.getKeyCode e)]
-    (cond
-      (= KeyEvent/VK_E k) (play-keyboard :c4)
-      (= KeyEvent/VK_T k) (play-keyboard :d4)
-      (= KeyEvent/VK_A k) (play-keyboard :e4)
-      (= KeyEvent/VK_O k) (play-keyboard :g4)
-      (= KeyEvent/VK_I k) (play-keyboard :a4)
-      (= KeyEvent/VK_N k) (play-keyboard :c3)
-      (= KeyEvent/VK_S k) (play-keyboard :d3)
-      (= KeyEvent/VK_H k) (play-keyboard :e3)
-      (= KeyEvent/VK_R k) (play-keyboard :g3)
-      (= KeyEvent/VK_D k) (play-keyboard :a3)
-      (= KeyEvent/VK_L k) (play-keyboard :c5)
-      (= KeyEvent/VK_C k) (play-keyboard :d5)
-      (= KeyEvent/VK_U k) (play-keyboard :e5)
-      (= KeyEvent/VK_M k) (play-keyboard :g5)
-      (= KeyEvent/VK_W k) (play-keyboard :a5)
-      (= KeyEvent/VK_F k) (play-keyboard :c2)
-      (= KeyEvent/VK_G k) (play-keyboard :d2)
-      (= KeyEvent/VK_Y k) (play-keyboard :e2)
-      (= KeyEvent/VK_P k) (play-keyboard :g2)
-      (= KeyEvent/VK_B k) (play-keyboard :a2)
-      (= KeyEvent/VK_V k) (play-keyboard :c6)
-      (= KeyEvent/VK_K k) (play-keyboard :d6)
-      (= KeyEvent/VK_J k) (play-keyboard :e6)
-      (= KeyEvent/VK_X k) (play-keyboard :g6)
-      (= KeyEvent/VK_Q k) (play-keyboard :a6)
-      (= KeyEvent/VK_Z k) (play-keyboard :c1)
-      )
-    )
-  )
+(defn notes [start]
+  (let [hang (map note [:c4 :d4 :e4 :g4 :a4 :c5])
+        intervals (cycle (map - (rest hang) hang))]
+  (reductions + start intervals)))
 
+(defn play [inst notes]
+  (inst (midi->hz (first notes)))
+  (Thread/sleep 500)
+  (recur inst (next notes)))
+
+(def notes (vec (take 10 (notes 70))))
+
+(def seed (rand-int 997))
+
+(defn map-note [n]
+  (play-keyboard (get notes (mod (* n seed) (- (count notes) 1)))))
 
 (defn myGlobalKeyListener []
   (reify
     NativeKeyListener
-    (nativeKeyPressed [this event] (map-note event))))
+    (nativeKeyPressed [this event] (map-note (.getKeyCode event)))))
 
 (defn -main [& args]
   (GlobalScreen/registerNativeHook)
