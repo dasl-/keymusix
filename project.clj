08e40f67ff61b349ca94c1a015e9b64473f20250
davidleibovic <david.leibovic@gmail.com>
2012-10-21 19:52:10 -0400

    added '^{:skip-aot true}' in project.clj to prevent startup issues when doing 'lein run'. without this, after a 'lein clean' you will have to do 'lein run' 3 times before it works properly

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :main keymusix.core
+  :main ^{:skip-aot true} keymusix.core
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [overtone "0.7.1"]
                  [org.clojars.pepijndevos/jnativehook "1.1.0_rc3"]])
