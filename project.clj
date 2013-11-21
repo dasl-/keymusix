effde9bf0d554fcf3b6779d1dad636c023f8d8f6
Pepijn de Vos <pepijndevos@gmail.com>
2013-11-21 19:44:55 +0100

    update jnativehook

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :main ^{:skip-aot true} keymusix.core
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [overtone "0.8.1"]
-                 [org.clojars.pepijndevos/jnativehook "1.1.0_rc3"]])
+                 [org.clojars.pepijndevos/jnativehook "1.1.4"]]
+  :main keymusix.core)
